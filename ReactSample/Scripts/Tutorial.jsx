ebb3a1f0b47ffb0aa57feaf13a419fa6940798aa
Narasaraju C <Narasa.Raju@sage.com>
2015-09-04 16:50:22 +0530

    Improved react performance

--- ReactSample/Scripts/Tutorial.jsx
+++ ReactSample/Scripts/Tutorial.jsx
@@ -19,60 +19,21 @@ var CommentBox = React.createClass({
   },
   componentDidMount: function() {
     this.loadCommentsFromServer();
-    //window.setInterval(this.loadCommentsFromServer, this.props.pollInterval);
   },
   render: function() {
     return (
-      <div className="commentBox">
-        <h1>Comments</h1>
-        <CommentList data={this.state.data} />
-        <CommentForm />
-      </div>
+	<ul className="list-group">
+            {this.state.data.map(function(x) {
+                return <li className="list-group-item">
+                <p>{x.Author}</p>
+                <p>{x.Text}</p>
+            </li>
+            })}
+    </ul>
     );
   }
 });
 
-var CommentList = React.createClass({
-  render: function() {
-    var commentNodes = this.props.data.map(function (comment) {
-      return (
-        <Comment author={comment.Author}>
-          {comment.Text}
-        </Comment>
-      );
-    });
-    return (
-      <div className="commentList">
-        {commentNodes}
-      </div>
-    );
-  }
-});
-
-var CommentForm = React.createClass({
-  render: function() {
-    return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
-    );
-  }
-});
-
-var Comment = React.createClass({
-  render: function() {
-    var converter = new Showdown.converter();
-    var rawMarkup = converter.makeHtml(this.props.children.toString());
-    return (
-      <div className="comment">
-        <h2 className="commentAuthor">
-          {this.props.author}
-        </h2>
-        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
-      </div>
-    );
-  }
-});
 
 React.render(
   <CommentBox url="/home/comments"/>,
